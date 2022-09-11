95b423f80aea061b5c2f4e0d7ce75fdb26d8b817
Muhammad Arif Wicaksana <muarwi@gmail.com>
2022-09-11 12:00:51 +0000

    added module controller

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
+	"github.com/wicaksana/go-simple-server/controller"
 	"github.com/wicaksana/go-simple-server/middleware"
 )
 
 func main() {
 	r := gin.New()
 	r.Use(middleware.JSONLogMIddleware())
+	r.Use(gin.Recovery())
 
 	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
 		log.Info().
 			Msg(fmt.Sprintf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers))
 	}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "Pong")
-	})
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, "Pong")
+	// })
+	controller.Router(r)
 
 	r.Run()
 }
